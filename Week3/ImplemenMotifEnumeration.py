318be65acf7bc26f7d0b7ddc5fdcd5d0072b978b
hedsw <yun.hyeok.lee@gmail.com>
2021-09-24 14:15:03 -0400

    Implement Motif Enumeration

--- Week3/ImplemenMotifEnumeration.py
+++ Week3/ImplemenMotifEnumeration.py
@@ -1,14 +1,60 @@
+
+class mismatches:
+    def mismatch(self, _setDNA, test, d):
+        _mismatches = 0
+        for i in range(len(_setDNA)):
+            if _mismatches <= d:
+                if _setDNA[i] != test[i]:
+                    _mismatches += 1
+            else:
+                break
+        if _mismatches <= d:
+            return True
+        else:
+            return False
+
+class motifs:
+    def motif(self, DNA, k, d):
+        shared = False
+        shares = 0
+        
+        for i in DNA:
+            found = False
+            for j in range(len(i), len(k)):
+                test = i[j:j+len(k)]
+                if mismatches.mismatch(k, test, d) <= d:
+                    print(k + " " + test)
+                    found = True
+                    shares += 1
+                    break
+            if found == False:
+                break
+            
+        if shares == len(DNA):
+            shared = True
+        return shared
+               
+
 class MotifEnumeration:
     def main(self, DNA, k, d):
-        _setDNA = {}
+        _setDNA = set()
         
-        _setDNA.add(DNA[0])
+        for i in DNA:        
+            for j in range(len(DNA[i])):
+                if j+3 <= len(DNA[i]):
+                    pattern = _setDNA[i:i+k]
+                    for m in range(len(_setDNA) - k):
+                        test = i[m:m+k]
+                        if mismatches.mismatch(pattern, test, d) and motifs.motif(DNA, test, d):
+                            _setDNA.add(test)
+                            
+                            
         # Strings
         print(k , d)
-        print(DNA)
+        #print(DNA)
+     
         
         
-
 if __name__ == "__main__":
     p = MotifEnumeration()
     DNA = ["ATTTGGC", "TGCCTTA", "CGGTATC", "GAAAATT"]
