00d07f0b44adb37b1c87f38b551520a1bb547b4e
hedsw <yun.hyeok.lee@gmail.com>
2021-10-14 14:02:28 -0400

    Eulerian is not easy

---
+++ Week5/EulerianCycle.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+
+    _str = "ADD DNA HERE"
+    k = 50
+    ans = []
+    for i in range(len(_str)):
+        if k + i <= len(_str):
+            ans.appenda_str[i:k+i])
+    
+    print('\n'.join(str(i) for i in sorted(ans)))
