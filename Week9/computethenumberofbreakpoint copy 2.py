8665f072228a59895d665fc977620105371a283c
hedsw <yun.hyeok.lee@gmail.com>
2021-12-05 10:01:52 -0500

    Rosa

--- Week9/computethenumberofbreakpoint copy 2.py
+++ Week9/computethenumberofbreakpoint copy 2.py
@@ -1,7 +1,8 @@
 def helper(data):
     count = 0
     for i in range(len(data) - 1):
-        if int(data[i]) != int(data[i+1]) - 1:
+        # Couting the near values in lists
+        if int(data[i]) != int(data[i+1]) - 1: # pick up only interger values 
             count += 1 
         else:
             continue
@@ -9,8 +10,10 @@ def helper(data):
         
 if __name__ == "__main__":
     with open('data.txt', 'r') as file:
+        # Read a line of data from a txt file.
         data_ = file.readline().strip().split()
         for i in range(len(data_)):
+            # Get Data from datasets
             data = [0] + data_ + [len(data_) + 1]
             
     helper(data)
